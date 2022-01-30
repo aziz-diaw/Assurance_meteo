3f1b6f678ef40818de931ea23f88a6fe7def4567
Aminata <ami95190@gmail.com>
2022-01-30 16:06:08 +0100

    lille et paris sur 20 ans, modif du fichier recup_data

--- data/recup_data.py
+++ data/recup_data.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
+from sklearn.neighbors import NearestCentroid
 
-years = ["2017","2018","2019","2020","2021"]
-#years = ["2019"]
+
+years = ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"]
+
 months = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
-#months = ["janvier"]
-towns = ["paris-5eme-tour-zamansky-jussieu","marseille-marignane-marseille-provence","lille-lesquin","nice-cote-d-azur","paris-montsouris"]
+towns = [["07156","paris-montsouris"],["07650","marseille-marignane-marseille-provence"],["07015","lille-lesquin"],["07690","nice-cote-d-azur"]]
 
 def extraction_donnes(month,year,town) -> list[list[str]]:
-    url = f"https://www.infoclimat.fr/climatologie-mensuelle/000BV/{month}/{year}/{town}.html"
+    url = f"https://www.infoclimat.fr/climatologie-mensuelle/{town[0]}/{month}/{year}/{town[1]}.html"
     page = requests.get(url)
     soup = BeautifulSoup(page.content,'html.parser')
     # l'objet soup, retourne la page html choisit
@@ -45,17 +46,19 @@ def extraction_donnes(month,year,town) -> list[list[str]]:
     #remplaceJourParMois(info,month)
     return info
 
-def data(name_file:str,year:str,towns:list[str]):
+def data(name_file:str,year:str,town:list[str]):
     info = []
     for month in months:
-        info.extend(extraction_donnes(month,year,towns[0]))
+        info.extend(extraction_donnes(month,year,town))
     with open(f'data/{name_file}', 'w', encoding='utf8', newline='',) as f:
         thewriter = writer(f)
-        header = ['Date', "Paris"]
+        header = ['Date', name_file] 
         thewriter.writerow(header)
         thewriter.writerows(info)
 
 if __name__ == "__main__":
-    for year in years:
-        name_file = f"paris_{year}.csv"
-        data(name_file,year,towns)
+    for town in towns:
+        name = town[1].split("-")[0]
+        for year in years:
+            name_file = f"{name}_{year}.csv"
+            data(name_file,year,town)
